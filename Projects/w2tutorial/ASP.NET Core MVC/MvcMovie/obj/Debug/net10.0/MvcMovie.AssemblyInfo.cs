4cb854e37cfd68cb73befb2b160552cd86a5ce84
hlaolil <hla24002@byui.edu>
2026-03-20 14:42:01 +0200

    add a view

--- Projects/w2tutorial/ASP.NET Core MVC/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ Projects/w2tutorial/ASP.NET Core MVC/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c6e38341a41b9d234dbe9395edea336dd4424c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6541f8b78bd6efe85b87654aa6f9448b76926927")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

52b4526b80fa889296c7e4d1d96942a1337043b9
hlaolil <hla24002@byui.edu>
2026-03-23 21:28:13 +0200

    complete todo

--- Projects/w3/Blazor/obj/Debug/net10.0/Blazor.AssemblyInfo.cs
+++ Projects/w3/Blazor/obj/Debug/net10.0/Blazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d04e89daaf9e24e32f5910152b78df90d816278d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d3d49acc650b10fd93e9cf6458d742aec888c5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

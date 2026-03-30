8008a3ac42f7c1a68768ace9be0b362b086c5279
hlaolil <hla24002@byui.edu>
2026-03-31 00:05:09 +0200

    init ConnectFour

--- Projects/w3/Blazor/obj/Debug/net10.0/Blazor.AssemblyInfo.cs
+++ Projects/w3/Blazor/obj/Debug/net10.0/Blazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d3d49acc650b10fd93e9cf6458d742aec888c5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c4a8bf3090a26fa2d819ea719b54c3c5f23612a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
